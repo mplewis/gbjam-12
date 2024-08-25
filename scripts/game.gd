38cd0b334c6385e5245fae843453b0083c1b852e
Matt Lewis <matt@mplewis.com>
2024-08-25 15:56:33 -0600

    feat: turbo

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,19 +1,25 @@
 class_name Game
 extends Node2D
 
-const FWD_RATE = 120
-const JUMP_RATE = 120
-const GRAVITY_RATE = 480
+const BASE_SPEED = 120
+const TURBO_MULT = 3.0
+const DECEL_STR = 240
+const JUMP_STR = 120
+const GRAVITY_STR = 480
+
 const WINDOW_SCALE = 8
 
 var button_states = {
 	"jump": false,
+	"turbo": false,
 }
 
+var player_fwd_vel: float = BASE_SPEED
 var player_up_vel := 0.0
 
 @onready var player: CharacterBody2D = $Player
 @onready var jump_sfx: AudioStreamPlayer2D = $Player/JumpSFX
+@onready var turbo_sfx: AudioStreamPlayer2D = $Player/TurboSFX
 
 @onready var floor_y := player.position.y
 
@@ -24,25 +30,46 @@ func _ready():
 
 
 func _process(delta):
-	player.position.x += FWD_RATE * delta
 	if Input.is_action_pressed("ui_gameboy_a"):
 		jump()
 	else:
 		button_states["jump"] = false
 
-	player_up_vel -= GRAVITY_RATE * delta
-	player.position.y -= player_up_vel * delta
-	if player.position.y > floor_y:
-		player.position.y = floor_y
+	if Input.is_action_pressed("ui_gameboy_b"):
+		turbo()
+	else:
+		button_states["turbo"] = false
+
+	player_up_vel -= GRAVITY_STR * delta
+	# player.position.y -= player_up_vel * delta
+	# if player.position.y > floor_y:
+	# 	player.position.y = floor_y
+	player.position.y = min(floor_y, player.position.y - player_up_vel * delta)
+
+	player.position.x += player_fwd_vel * delta
+	if player_fwd_vel > BASE_SPEED:
+		player_fwd_vel = max(BASE_SPEED, player_fwd_vel - DECEL_STR * delta)
 
 
 func jump():
+	if player.position.y != floor_y:
+		return
+
 	if button_states["jump"]:
 		return
 	button_states["jump"] = true
 
-	if player.position.y != floor_y:
+	jump_sfx.play()
+	player_up_vel = JUMP_STR
+
+
+func turbo():
+	if player_fwd_vel > BASE_SPEED:
 		return
 
-	jump_sfx.play()
-	player_up_vel = JUMP_RATE
+	if button_states["turbo"]:
+		return
+	button_states["turbo"] = true
+
+	turbo_sfx.play()
+	player_fwd_vel *= TURBO_MULT
