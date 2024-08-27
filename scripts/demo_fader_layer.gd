49b6dc135c5404c0a725e81fb00b58bddb25a71a
Matt Lewis <matt@mplewis.com>
2024-08-26 22:58:05 -0600

    feat: demo fader layer

---
+++ scripts/demo_fader_layer.gd
@@ -0,0 +1,12 @@
+class_name DemoFaderLayer
+extends CanvasLayer
+
+const FADE_DURATION_SEC = 2.0
+
+var fade_in = false
+
+@onready var r: ColorRect = $ColorRect
+
+
+func _process(_delta):
+	r.color.a = 0.5 + 0.5 * sin(Time.get_ticks_msec() / (FADE_DURATION_SEC * 1000.0) * PI)
