0b521c387fa1f5c8ff3645def677b9ed050ca874
Matt Lewis <matt@mplewis.com>
2024-08-28 23:03:39 -0600

    tidy: remove useless home scene

--- scripts/scene_mgr.gd
+++ scripts/scene_mgr.gd
@@ -1,16 +1,18 @@
 extends Node
 
-var scene_names: Array[String] = []
-
 const WINDOW_SCALE = 8
+
 var resized = false
+var scene_paths: Array[String] = []
 
 
 func _ready():
 	set_appropriate_window_size()
+	scene_paths.push_back(ProjectSettings.get_setting("application/run/main_scene"))
 
 
-## Singleton callable to set the window size for
+## Singleton callable to resize the window appropriately for desktop development.
+## This is safe to call in every scene's `ready` method.
 func set_appropriate_window_size() -> void:
 	if resized:
 		return
@@ -21,22 +23,18 @@ func set_appropriate_window_size() -> void:
 	resized = true
 
 
+## Open a scene in `scenes/` by its short name (i.e. not by full `res://` path)
 func open(scene_name: String) -> void:
-	scene_names.push_back(scene_name)
-	var path := _path_for(scene_name)
-	print("Changing to %s" % path)
+	var path := "res://scenes/%s.tscn" % scene_name
+	scene_paths.push_back(path)
 	get_tree().change_scene_to_file(path)
 
 
+## Close the current scene and return to the previous one
 func close() -> void:
-	if scene_names.size() < 2:
-		print_debug("No scene_names remaining")
+	if scene_paths.size() < 2:
+		print_debug("No scenes remaining")
 		return
 
-	scene_names.pop_back()
-	var prev_scene_name = scene_names.back()
-	get_tree().change_scene_to_file(_path_for(prev_scene_name))
-
-
-func _path_for(scene_name: String) -> String:
-	return "res://scenes/%s.tscn" % scene_name
+	scene_paths.pop_back()
+	get_tree().change_scene_to_file(scene_paths.back())
