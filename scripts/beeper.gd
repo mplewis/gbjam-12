9d5bf970d4d9ed42f7cc8fccc18428d893935d58
Matt Lewis <matt@mplewis.com>
2024-08-31 15:16:12 -0600

    feat: beeper

---
+++ scripts/beeper.gd
@@ -0,0 +1,93 @@
+class_name Beeper
+extends Node
+
+signal on_beat(beat: int, end_of_seq: bool)
+
+@export_range(1, 400) var bpm: float = 120
+@export var samples: Array[AudioStream] = []
+
+var sample_nodes: Array[AudioStreamPlayer] = []
+## Array[Array[int]]
+var notes := []
+var seq_dur_sec := 0.0
+var beat_dur_sec := 0.0
+
+var loop := false
+var playing := false
+var seq_ts := 0.0
+var beat_ts := 0.0
+var beat := 0
+
+# HACK: time gets wonky on the first play
+var first = false
+
+
+func _process(delta):
+	if !playing:
+		return
+
+	if first:
+		first = false
+	else:
+		seq_ts += delta
+		beat_ts += delta
+
+	# HACK: non-deterministic delta shifting sometimes leads to "too many beats" right before a loop
+	if beat_ts >= beat_dur_sec && beat < notes.size():
+		beat_ts = fmod(beat_ts, beat_dur_sec)
+		on_beat.emit(beat, seq_ts >= seq_dur_sec)
+		var to_play = notes[beat]
+		for i in range(to_play.size()):
+			sample_nodes[to_play[i]].play()
+		beat += 1
+
+	if seq_ts >= seq_dur_sec:
+		if loop:
+			seq_ts = fmod(seq_ts, seq_dur_sec)
+			beat = 0
+		else:
+			playing = false
+
+
+func _update_local_song():
+	if notes.size() == 0:
+		return
+	seq_dur_sec = 60.0 / bpm * notes.size()
+	beat_dur_sec = seq_dur_sec / notes.size()
+
+	for i in range(sample_nodes.size()):
+		var existing_node := sample_nodes[i]
+		if existing_node:
+			existing_node.stop()
+			existing_node.queue_free()
+
+	sample_nodes = []
+
+	for i in range(samples.size()):
+		var new_node := AudioStreamPlayer.new()
+		new_node.stream = samples[i]
+		sample_nodes.append(new_node)
+		add_child(new_node)
+
+
+func configure(newNotes: Array, looping = true):
+	notes = newNotes
+	loop = looping
+	_update_local_song()
+
+
+func play():
+	playing = true
+
+
+func stop():
+	playing = false
+	seq_ts = 0
+	beat_ts = 0
+	beat = 0
+	for i in range(sample_nodes.size()):
+		sample_nodes[i].stop()
+
+
+func fmod(a: float, b: float) -> float:
+	return a - b * floor(a / b)
--- scripts/danceoff.gd
+++ scripts/danceoff.gd
@@ -1,102 +1,36 @@
 class_name Danceoff
 extends Node2D
 
-enum Note { A, B, REST }
+@onready var beeper: Beeper = $Beeper
 
-## Track beats per minute
-const BPM: float = 240
-## Bars in the track
-const BARS: int = 4
-## Beats per bar
-const BEATS_PER_BAR: int = 4
-## How much of the song is the generator allowed to use?
-const GEN_RATIO: float = 0.5
-
-var song_duration_sec := (60.0 * BARS * BEATS_PER_BAR) / BPM
-var beat_duration_sec := 60.0 / BPM
-var song_ts := 0.0
-var beat_ts := 0.0
-var beat := 0
-var first := true
-
-var goal_notes: Array[Note] = []
-
-@onready var bgm: AudioStreamPlayer = $BGM
-@onready var beep1: AudioStreamPlayer = $Beep1
-@onready var beep2: AudioStreamPlayer = $Beep2
-@onready var debug_notes: Label = $DebugNotes
-@onready var debug_pos: Label = $DebugPos
+const demo_song = [
+	[0, 1],
+	[],
+	[2],
+	[1],
+	[],
+	[1],
+	[2],
+	[],
+	[],
+	[],
+	[],
+	[],
+	[],
+	[],
+	[],
+	[],
+]
 
 
 func _ready():
 	SceneMgr.set_appropriate_window_size()
 	GBtn.on_start.connect(SceneMgr.close)
-	GBtn.on_a.connect(_on_a)
-	GBtn.on_b.connect(_on_b)
-	loop()
+
+	beeper.configure(demo_song)
+	beeper.play()
+	beeper.on_beat.connect(_on_beat)
 
 
-func _process(delta):
-	if first:
-		first = false
-	else:
-		song_ts += delta
-		beat_ts += delta
-	if song_ts >= song_duration_sec:
-		song_ts = float_mod(song_ts, song_duration_sec)
-		loop()
-	if beat_ts >= beat_duration_sec:
-		beat = (beat + 1) % BEATS_PER_BAR
-		beat_ts = float_mod(beat_ts, beat_duration_sec)
-		if goal_notes:
-			match goal_notes.pop_front():
-				Note.A:
-					_on_a()
-				Note.B:
-					_on_b()
-				Note.REST:
-					pass
-
-	print(beat)
-	if beat < goal_notes.size():
-		var pos_chars := []
-		for i in range(goal_notes.size()):
-			pos_chars.push_back(" ")
-		pos_chars[beat] = "^"
-		debug_pos.text = " ".join(pos_chars)
-		print(pos_chars)
-
-
-func loop():
-	goal_notes = generate_beats()
-	debug_notes.text = " ".join(goal_notes)
-	bgm.stop()
-	bgm.play()
-
-
-func generate_beats() -> Array[Note]:
-	var notes: Array[Note] = []
-	var count := int(BARS * BEATS_PER_BAR * GEN_RATIO)
-	for i in range(count):
-		match randi() % 3:
-			0:
-				notes.append(Note.A)
-			1:
-				notes.append(Note.B)
-			2:
-				notes.append(Note.REST)
-	return notes
-
-
-func _on_a():
-	beep1.stop()
-	beep1.play()
-
-
-func _on_b():
-	beep2.stop()
-	beep2.play()
-
-
-func float_mod(a: float, b: float) -> float:
-	return a - b * floor(a / b)
+func _on_beat(beat: int, end_of_seq: bool):
+	print("beat: %d, end_of_seq: %s" % [beat, str(end_of_seq)])
