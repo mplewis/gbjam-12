265381a6683dd300fe13c25da8a225eac396006b
Matt Lewis <matt@mplewis.com>
2024-09-02 22:13:58 -0600

    wip: moving arrows with sync

---
+++ scripts/arrow.gd
@@ -0,0 +1,35 @@
+class_name Arrow
+extends RigidBody2D
+
+const TIME_AFTER_GOAL_SEC = 0.5
+
+@export var goal: Node2D
+@export var duration_to_goal_sec: float = 1.0
+@export var active: bool = false
+
+var alive_sec: float = 0.0
+var lifetime_sec: float = duration_to_goal_sec + TIME_AFTER_GOAL_SEC
+
+var velocity: Vector2
+
+
+func _to_string():
+	return "<Arrow -> %s>" % goal.name
+
+
+func _physics_process(delta):
+	if !active:
+		return
+
+	if alive_sec >= lifetime_sec:
+		queue_free()
+		return
+
+	alive_sec += delta
+	var dist = global_position.distance_to(goal.global_position)
+	if dist > 1.0: # Allow arrows to fly past the goal
+		var remain_sec = duration_to_goal_sec - alive_sec
+		var speed = dist / remain_sec * delta
+		var dir = (goal.global_position - global_position).normalized()
+		velocity = dir * speed
+	move_and_collide(velocity)
--- scripts/dance.gd
+++ scripts/dance.gd
@@ -2,6 +2,11 @@ class_name Dance
 extends Node2D
 
 @onready var midi_player: MidiPlayer = $MidiPlayer
+@onready var ArrowL: Arrow = $ArrowL
+@onready var ArrowC: Arrow = $ArrowC
+@onready var ArrowR: Arrow = $ArrowR
+
+@onready var arrows = [ArrowL, ArrowC, ArrowR]
 
 
 func _ready():
@@ -10,6 +15,10 @@ func _ready():
 
 	midi_player.midi_event.connect(_on_midi_event)
 	midi_player.seek(2050.0)
+
+
+func _physics_process(_delta):
+	pass
 
 
 func _on_start():
@@ -23,5 +32,7 @@ func _on_midi_event(channel, event):
 	if channel.number != 2:
 		return
 
-	var arrow_lcr = event.note % 3
-	print(arrow_lcr)
+	var tmpl = arrows[event.note % len(arrows)]
+	var arrow = tmpl.duplicate()
+	add_child(arrow)
+	arrow.active = true
