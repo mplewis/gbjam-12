28e903886c085de359cd15cb5b8b5d7f0652f992
Matt Lewis <matt@mplewis.com>
2024-09-02 21:36:22 -0600

    wip: read midi and generate notes from track

--- scripts/dance.gd
+++ scripts/dance.gd
@@ -1,11 +1,27 @@
 class_name Dance
-extends Control
+extends Node2D
+
+@onready var midi_player: MidiPlayer = $MidiPlayer
 
 
 func _ready():
 	SceneMgr.set_appropriate_window_size()
 	GBtn.on_start.connect(_on_start)
 
+	midi_player.midi_event.connect(_on_midi_event)
+	midi_player.seek(2050.0)
+
 
 func _on_start():
 	SceneMgr.close()
+
+
+func _on_midi_event(channel, event):
+	if event.type != SMF.MIDIEventType.note_on:
+		return
+
+	if channel.number != 2:
+		return
+
+	var arrow_lcr = event.note % 3
+	print(arrow_lcr)
