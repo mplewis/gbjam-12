d8ff9abeb28a7141da99ce0e700f3f0cd7b075c2
Matt Lewis <matt@mplewis.com>
2024-09-17 17:28:27 -0600

    wip: working collision

--- scripts/candy_arrow_follower.gd
+++ scripts/candy_arrow_follower.gd
@@ -1,32 +1,50 @@
 class_name CandyArrowFollower
 extends PathFollow2D
 
-@onready var up_sprite = $Up
+@onready var hitbox: CollisionShape2D = $Body/Hitbox
+@onready var up_sprite: AnimatedSprite2D = $Up
 @onready var down_sprite: AnimatedSprite2D = $Down
 @onready var left_sprite: AnimatedSprite2D = $Left
 @onready var right_sprite: AnimatedSprite2D = $Right
 
+var dirs = ["U", "D", "L", "R"]
 @onready var sprites = [up_sprite, down_sprite, left_sprite, right_sprite]
+
+## Goal position is based on the spawner's `progress` position
+@onready var goal_progress_ratio := progress_ratio
+## The target path onto which to spawn candies
 @export var path: Path2D
 
-var template = true
+var template := true
 var rate_per_sec := 0.0  # how much to add to goal_pos per second
+var dir_str := "?"
 
 
 func _ready():
 	assert(path, "Path to spawn onto must be set")
+	progress = 0.0
 
 	if template:
 		return
 
-	progress = 0.0
 	for s in sprites:
 		if s.visible:
 			s.play()
 
 
+func _process(delta):
+	if template:
+		return
+
+	progress_ratio = min(progress_ratio + rate_per_sec * delta, 1.0)
+	if progress_ratio >= 1.0:
+		queue_free()
+
+	# HACK: For some reason the hitbox does not follow this node correctly.
+	hitbox.global_position = global_position
+
+
 func spawn(dir: int, to_hit_sec: float) -> CandyArrowFollower:
-	print(dir)
 	var sprite: AnimatedSprite2D = sprites[dir]
 	assert(sprite, "Invalid direction: %d" % dir)
 	for s in sprites:
@@ -37,10 +55,11 @@ func spawn(dir: int, to_hit_sec: float) -> CandyArrowFollower:
 
 	var x = duplicate()
 	x.template = false
+	x.dir_str = dirs[dir]
+	x.name = "CandyArrow"
 
 	x.global_position = global_position
-	# Goal position is based on the spawner's `progress` position
-	x.rate_per_sec = progress_ratio / to_hit_sec
+	x.rate_per_sec = goal_progress_ratio / to_hit_sec
 
 	path.add_child(x)
 	x.show()
@@ -48,10 +67,5 @@ func spawn(dir: int, to_hit_sec: float) -> CandyArrowFollower:
 	return x
 
 
-func _process(delta):
-	if template:
-		return
-
-	progress_ratio = min(progress_ratio + rate_per_sec * delta, 1.0)
-	if progress_ratio >= 1.0:
-		queue_free()
+func punt():
+	print("Punt not yet implemented")
